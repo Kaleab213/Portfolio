e256a33e6a93bc3b63ac583a19b349b903fd36b5
unknown <kaleabtibebu400@gmail.com>
2023-09-28 16:58:05 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App.css'; // Import your root component here
+import App from './src/App.js'; // Import your root component here
 
 ReactDOM.render(
   <React.StrictMode>
